fd735973fc9b8956a3d90f6e352d7d4a291b95ca
hopwesley <ws@sinoc.io>
2025-02-03 18:03:53 +0800

    init mac local app

---
+++ local_app/macos/bmail/bmail/bmailApp.swift
@@ -0,0 +1,32 @@
+//
+//  bmailApp.swift
+//  bmail
+//
+//  Created by wesley on 2025/2/3.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct bmailApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
